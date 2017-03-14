dfb371f8575add995628ee367206547f5c01ee5c
meltem kilic <melltemkilic@gmail.com>
2017-03-14 01:06:44 +0000

    fetching the images using a second function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 var GITHUB_USER = "turquoisemelon";
 var GITHUB_TOKEN = "ef7403e12556166fad254891cb10bd700c1e5e37";
@@ -10,39 +11,24 @@ function getRepoContributors(repoOwner, repoName, callback) {
     headers: {
       'User-Agent': 'GitHub Avatar Downloader - Student Project'
     }
-};
+  };
 
-request.get(options, function(err, response) {
-  if (err) throw error;
-  callback(response);
+  request.get(options, function(err, response) {
+    if (err) throw error;
+    callback(response);
   });
 }
 
 function callback (response) {
-
   var results = JSON.parse(response.body);
-
   for (var prop in results) {
-    console.log(results[prop]['avatar_url']);
+    var filePath = './' + results[prop]['login'] + '.jpg';
+    downloadImageByURL(results[prop]['avatar_url'], filePath);
   }
 }
 
+function downloadImageByURL (url ,filePath) {
+  request.get(url).pipe(fs.createWriteStream(filePath));
+}
 
 getRepoContributors("jquery", "jquery", callback);
-
-
-// request.get('https://sytantris.github.io/http-examples/future.jpg')
-//        .on('error', function (err) {
-//          throw err;
-//        })
-//        .on('response', function (response) {
-//          console.log('Response Status Code: ', response.statusCode);
-//          console.log('Response Status Message: ', response.statusMessage);
-//          console.log('Response Content Type: ', response.headers['content-type']);
-//          console.log('Downloading image...');
-//        })
-//        .on('end', function (response){
-//          console.log('Download complete.');
-//        })
-//        .pipe(fs.createWriteStream('./future.jpg'))
-//        .end();
